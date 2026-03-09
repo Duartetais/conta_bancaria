4162115a8247b24d9a61f609ace808bf6e4e5cf5
Duartetais <tahysduarte22@gmail.com>
2026-03-09 12:14:23 -0300

    Projeto conta Bancria

---
+++ src/util/colors.ts
@@ -0,0 +1,19 @@
+export class Colors {
+    // Reset - Limpa a formatação
+    public static readonly RESET: string = "\u001b[0m";
+
+    // Cores do Texto (Foreground)
+    public static readonly TEXT_RED: string = "\u001b[31m";
+    public static readonly TEXT_GREEN: string = "\u001b[32m";
+    public static readonly TEXT_YELLOW: string = "\u001b[33m";
+    public static readonly TEXT_BLUE: string = "\u001b[34m";
+    public static readonly TEXT_PURPLE: string = "\u001b[35m";
+    public static readonly TEXT_CYAN: string = "\u001b[36m";
+    public static readonly TEXT_WHITE: string = "\u001b[37m";
+
+    // Cores de Fundo (Background)
+    public static readonly BG_RED: string = "\u001b[41m";
+    public static readonly BG_GREEN: string = "\u001b[42m";
+    public static readonly BG_YELLOW: string = "\u001b[43m";
+    public static readonly BG_BLUE: string = "\u001b[44m";
+}
